132b0972ed8682c2282ae7fc1dacf2ce46284431
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-03-09 19:10:01 +0800

    feat: nicknames and rooms working - hardcoded

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(cors())
 
 const server = http.createServer(app)
 
+let users = []
+
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:5173",
@@ -19,6 +21,22 @@ const io = new Server(server, {
 
     }
 })
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    socket.on('send-nickname', function(nickname) {
+        socket.nickname = nickname
+        users.push(socket.nickname)
+        console.log(users)
+        socket.emit('users-list', users)
+    })
+    socket.on('join-room', function(room) {
+        socket.join(room)
+        console.log(room)
+    })
+})
+
+
 
 server.listen(3001, () => {
     console.log('server running')
